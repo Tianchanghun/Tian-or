c982008608f4dac0a8261b30730e852c30c01beb
Tianchanghun <splash76@gmail.com>
2022-07-06 16:10:21 +0900

    작업물 업로드 2차

--- board/urls.py
+++ board/urls.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import board.views
 from . import views
 
 admin.site.site_header = 'AONE HEADER'
 admin.site.site_title = 'AONE TITLE'
 admin.site.index_title = 'SITE INDEX'
 
+app_name = 'board'
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('board/notice', board.views.notice, name='notice'),
-    path('board/news', board.views.news, name='news'),
-    path('join/', include('join.urls')),
+
+    path('board/notice', views.notice, name='notice'),
+    path('board/news', views.news, name='news')
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
